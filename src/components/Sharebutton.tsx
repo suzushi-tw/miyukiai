9c45de6c4ec2787e7f3b3ac18aced0ad7a2093d4
suzushi-tw <ianhuang2023a@gmail.com>
2025-05-01 17:51:47 +0200

    fix padding

--- src/components/Sharebutton.tsx
+++ src/components/Sharebutton.tsx
@@ -25,7 +25,7 @@ export function ShareButton({ modelId }: ShareButtonProps) {
     <Button
       variant="outline"
       size="lg"
-      className="w-full"
+      className="w-full pt-3"
       onClick={handleCopyLink}
     >
       {isCopied ? (
